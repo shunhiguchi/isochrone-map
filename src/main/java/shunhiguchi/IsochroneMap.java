c2b9bc9eae8315308b15a36b64d0dc43ce1fe8ee
Shun Higuchi <shnhgch.acc@outlook.com>
2021-10-09 21:26:20 -0400

    Set up project

---
+++ src/main/java/shunhiguchi/IsochroneMap.java
@@ -0,0 +1,46 @@
+package shunhiguchi;
+
+public class IsochroneMap {
+    /**
+     * Identify nodes accessible within a threshold distance.
+     * @param args
+     */
+    public IsochroneMap() {
+        /**
+         * Define a threshold distance.
+         */
+
+        /**
+         * Load nodes and edges from input files.
+         */
+
+        /**
+         * Remove nodes and edges outside a threshold distance.
+         */
+
+        /**
+         * Compute the shortest path distances to all nodes.
+         */
+
+        /**
+         * Mark nodes with their shortest path distances within the threshold
+         * distance. Mark edges used as part of the shortest paths.
+         */
+
+        /**
+         * Export the nodes and edges.
+         */
+
+        /**
+         * Visualize the isochrone map.
+         */
+    }
+
+    /**
+     * Test client.
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        ;
+    }
+}
