a8464ac2da96889a5bd083c2058e85fefcb6bfae
Shun Higuchi <shnhgch.acc@outlook.com>
2021-10-12 22:08:57 -0400

    Update ShortestPath.java

--- src/main/java/shunhiguchi/ShortestPath.java
+++ src/main/java/shunhiguchi/ShortestPath.java
@@ -1,4 +1,12 @@
 package shunhiguchi;
 
 public class ShortestPath {
+    /**
+     * Dijkstra's shortest path algorithm.
+     */
+    public ShortestPath(Diagraph G, int s) {
+        for (edge e: G.edges()) {
+            if (e.)
+        }
+    }
 }
