07d45702672a16ad2bddd3b7d184ae5985ed0ffc
Shun Higuchi <shnhgch.acc@outlook.com>
2021-11-20 15:39:24 -0500

    Update the sample diagraph

--- src/main/java/shunhiguchi/ShortestPath.java
+++ src/main/java/shunhiguchi/ShortestPath.java
@@ -89,6 +89,44 @@ public class ShortestPath {
             }
         }
     }
+
+    public static void main(String arg[]) {
+        int V = 5;
+        int source = 0;
+
+        // Adjacency list representation of the
+        // connected edges by declaring List class object
+        // Declaring object of type List<Node>
+        List<List<Node> > adj
+                = new ArrayList<List<Node> >();
+
+        // Initialize list for every node
+        for (int i = 0; i < V; i++) {
+            List<Node> item = new ArrayList<Node>();
+            adj.add(item);
+        }
+
+        // Inputs for the GFG(dpq) graph
+        adj.get(0).add(new Node(1, 9));
+        adj.get(0).add(new Node(2, 6));
+        adj.get(0).add(new Node(3, 5));
+        adj.get(0).add(new Node(4, 3));
+
+        adj.get(2).add(new Node(1, 2));
+        adj.get(2).add(new Node(3, 4));
+
+        // Calculating the single source shortest path
+        GFG dpq = new GFG(V);
+        dpq.dijkstra(adj, source);
+
+        // Printing the shortest path to all the nodes
+        // from the source node
+        System.out.println("The shorted path from node :");
+
+        for (int i = 0; i < dpq.dist.length; i++)
+            System.out.println(source + " to " + i + " is "
+                    + dpq.dist[i]);
+    }
 }
 
 /**
