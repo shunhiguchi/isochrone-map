e2524692d3113bb1a5f66139d4678ce1ee6bb16f
Shun Higuchi <shnhgch.acc@outlook.com>
2021-11-27 19:10:13 -0500

    Update Visualizer.java

--- src/main/java/Visualizer.java
+++ src/main/java/Visualizer.java
@@ -1,4 +1,29 @@
-import java.awt.
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Visualizer {
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class Visualizer extends JComponent {
+    private int width;
+    private int height;
+    public Visualizer (int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    protected void drawEdge(Graphics g) {
+        
+    }
+    public static void main(String[] args) {
+        int w = 640;
+        int h = 480;
+
+        JFrame f = new JFrame();
+        f.setSize(w, h);
+        f.setTitle("Isochrone Map");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 }
