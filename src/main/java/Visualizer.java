26f55241029bf496b55f4a65097d423457abec55
Shun Higuchi <shnhgch.acc@outlook.com>
2021-11-27 21:18:28 -0500

    Update Visualizer.java
    Update Edge class

--- src/main/java/Visualizer.java
+++ src/main/java/Visualizer.java
@@ -1,10 +1,11 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.Line2D;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -19,6 +20,7 @@ public class Visualizer extends JComponent {
         this.height = height;
     }
 
+    @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         RenderingHints rh = new RenderingHints(
@@ -51,17 +53,18 @@ public class Visualizer extends JComponent {
 }
 
 class Vertex {
-    private double x;
-    private double y;
-    private boolean within;
-    private boolean source;
+    private final double x;
+    private final double y;
+    private final boolean within;
+    private final boolean source;
 
     private static final int R = 50;
     private static final Color color = new Color(100, 149, 237);
 
-    public Vertex(int x, int y, boolean within, boolean source) {
+    public Vertex(int x, int y, int id, boolean within, boolean source) {
         this.x = x - (double) R / 2;
         this.y = y - (double) R / 2;
+        this.id = id;
         this.within = within;
         this.source = source;
     }
@@ -70,5 +73,37 @@ class Vertex {
         Ellipse2D.Double e = new Ellipse2D.Double(this.x, this.y, R, R);
         g2d.setColor(color);
         g2d.fill(e);
+    }
+}
+
+class Edge {
+    private final int x1;
+    private final int y1;
+    private final int x2;
+    private final int y2;
+    private final boolean used;
+
+    private static final Color color = new Color(100, 149, 237);
+
+    private static final BasicStroke bsDefault = new BasicStroke();
+
+    private float[] dash = {2f, 0f, 2f};
+    private static final BasicStroke bsDashed = new BasicStroke(
+            BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1.0f, dash, 2f
+    );
+
+    public Edge(int x1, int y1, int x2, int y2, boolean used) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.used = used;
+    }
+
+    public void drawEdge(Graphics2D g2d) {
+        Line2D.Double line = new Line2D.Double(this.x1, this.y1, this.x2, this.y2);
+        if (used) g2d.setStroke(bsDefault); else g2d.setStroke(bsDashed);
+        g2d.setColor(color);
+        g2d.draw(line);
     }
 }
