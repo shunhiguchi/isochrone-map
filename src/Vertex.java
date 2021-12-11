40039cf2b31c59893c4a5a592679c8728ab03fcb
Shun Higuchi <shnhgch.acc@outlook.com>
2021-12-11 12:01:22 -0500

    Update Vertex.java

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,45 +1,36 @@
+/**
+ * The Vertex class represents a vertex in a directed digraph. In addition, it
+ * contains methods and attributes for isochrone map visualization.
+ */
 public class Vertex {
 
-    /*
-     * radius of a circle representing the vertex.
-     */
+    /* Radius of a circle representing the vertex. */
     private static final int R = 40;
 
-    /*
-     * id of the vertex.
-     */
+    /* id of the vertex. */
     public final int id;
 
-    /*
-     * x coordinate of the vertex.
-     */
+    /* x coordinate of the vertex. */
     public final int x;
 
-    /*
-     * y coordinate of the vertex.
-     */
+    /* y coordinate of the vertex. */
     public final int y;
 
-    /*
-     * the vertex is a source vertex.
-     */
+    /* the vertex is a source vertex. */
     public boolean isSource;
 
-    /*
-     * the vertex is reachable within the threshold cost.
-     */
+    /* The vertex is reachable within the threshold cost. */
     public boolean isReachable;
 
-    /*
-     * x coordinate of the top left corner of the vertex bound shape.
-     */
+    /* x coordinate of the top left corner of the vertex bound shape. */
     private final float xtl;
 
-    /*
-     * y coordinate of the top left corner of the vertex bound shape.
-     */
+    /* y coordinate of the top left corner of the vertex bound shape. */
     private final float ytl;
 
+    /*
+     * Constructor.
+     */
     public Vertex(int id, int x, int y) {
         this.id = id;
         this.x = x;
